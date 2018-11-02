6ec3b75dd8a38d869449606dfa2fdddbc4e40df6
privateidaho99 <bendemor@byu.net>
2018-11-02 04:24:17 +0000

    added first

---
+++ cfb/routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var fs = require('fs'), obj;
+var router = express.Router();
+var request = require("request");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+
+
+router.get('/getteam', function(req, res, next) {
+    fs.readFile('./public/test.json', 'utf8', function (err, data) {
+        if (err) throw err
+        obj = JSON.parse(data);
+        
+        var arrayIndex = Math.floor(Math.random() * Object.keys(obj.schools).length);
+        
+        console.log(arrayIndex);
+        var name = obj.schools[arrayIndex].name;
+        var mascot = obj.schools[arrayIndex].mascot;
+        var pic = obj.schools[arrayIndex].pic;
+        
+        console.log(name);
+        console.log(mascot);
+        console.log(pic);
+        
+        var jsonresult = [name, mascot, pic];
+        
+        res.status(200).json(jsonresult);
+    });
+});   
+
+module.exports = router;
